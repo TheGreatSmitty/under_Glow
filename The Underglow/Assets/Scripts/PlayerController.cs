48712b70c58a5b6e06457ee4f85417366405f5bc
Ricky110926 <rduon451@live.kutztown.edu>
2020-11-17 16:03:17 -0500

    Player Movement
    Was able to set up the player movement and the animation

---
+++ The Underglow/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rigi;
+    private Collider2D collison;
+
+    public float speed;
+    private Vector2 direction;
+
+    public void Start()
+    {
+        rigi = GetComponent<Rigidbody2D>();
+        collison = GetComponent<Collider2D>();
+    }
+
+    public void FixedUpdate()
+    {
+        if (Input.GetAxisRaw("Horizontal") < 0 || Input.GetAxisRaw("Horizontal") > 0)
+        {
+            direction.x = speed * Input.GetAxis("Horizontal");
+        }else
+        {
+            direction.x = 0;
+        }
+
+        rigi.MovePosition(rigi.position + direction * Time.fixedDeltaTime);
+    }
+}
