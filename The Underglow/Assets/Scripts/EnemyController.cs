bb0881e8b1a5183ccbb2215dfdcbc59554c9c465
Ricky110926 <rduon451@live.kutztown.edu>
2020-12-01 17:02:20 -0500

    Update EnemyController.cs
    slight update

--- The Underglow/Assets/Scripts/EnemyController.cs
+++ The Underglow/Assets/Scripts/EnemyController.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    private Rigidbody2D rigi;
+    private Vector2 direction;
+    public float speed;
+
     // Start is called before the first frame update
     void Start()
+    {
+        rigi = GetComponent<Rigidbody2D>();
+    }
+
+    public void Update()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            //Change direction
+        }
     }
 }
