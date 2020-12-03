3486295306c89dd22b5bf19010a4b54db50b8cba
TheGreatSmitty <73313393+TheGreatSmitty@users.noreply.github.com>
2020-12-03 06:08:16 -0500

    Create collisions.cs

---
+++ The Underglow/Assets/Scripts/collisions.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisions : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+    }
+}
