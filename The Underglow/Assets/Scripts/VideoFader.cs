01f37ca804c45b4d04b2a683689e04e00c46377c
Ricky110926 <rduon451@live.kutztown.edu>
2020-12-05 16:41:39 -0500

    Collsion and Video Update
    Splash Screens got updated and are now automatically switching. Collision of the level was made through edge collisions

---
+++ The Underglow/Assets/Scripts/VideoFader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VideoFader : MonoBehaviour
+{
+    public float wait_time = 6f;
+    public int index;
+    private void Start()
+    {
+        StartCoroutine(Wait_For_Video());
+    
+    }
+
+    IEnumerator Wait_For_Video()
+    {
+        yield return new WaitForSeconds(wait_time);
+
+        SceneManager.LoadScene(index);
+    }
+}
