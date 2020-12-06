1f80c36105e7297d1daf129322b7def90ffbbcd0
Ricky110926 <rduon451@live.kutztown.edu>
2020-12-06 18:11:14 -0500

    Final Puish
    I did my best

---
+++ The Underglow/Assets/DialogueTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public GameObject child;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        print(collision);
+        if (collision.gameObject.CompareTag("Player")) 
+        child.SetActive(true);
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            child.SetActive(false);
+    }
+}
--- The Underglow/Assets/Scripts/PlayerController.cs
+++ The Underglow/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class PlayerController : MonoBehaviour
 {
@@ -8,20 +9,25 @@ public class PlayerController : MonoBehaviour
     private Collider2D collison;
     
     private SpriteRenderer childobject;
+    private Animator anim;
 
     public float speed;
     private Vector2 direction;
 
+    private bool ground;
     //Health
     public int maxHealth = 20;
     public int currentHealth;
     public HealthBar healthBar;
+
+    bool damageTime;
     public void Start()
     {
         rigi = GetComponent<Rigidbody2D>();
         collison = GetComponent<Collider2D>();
         childobject = GetComponentInChildren<SpriteRenderer>();
-
+        anim = GetComponentInChildren<Animator>();
+        damageTime = false;
         currentHealth = maxHealth;
         healthBar.SetHealth(maxHealth);
     }
@@ -30,6 +36,7 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetAxisRaw("Horizontal") < 0 || Input.GetAxisRaw("Horizontal") > 0)
         {
+            if (damageTime != true)  anim.Play("HeroWalkCycle_Right");
             if (Input.GetAxisRaw("Horizontal") < 0){ childobject.flipX = true; }
             else { childobject.flipX = false; }
             direction.x = speed * Input.GetAxis("Horizontal");
@@ -40,14 +47,41 @@ public class PlayerController : MonoBehaviour
 
         rigi.MovePosition(rigi.position + direction * Time.fixedDeltaTime);
     }
+    public void Update()
+    {
+        if (currentHealth <= 0)
+        {
+            anim.Play("Hero Die");
+        }
 
+        if (ground)
+        {
+            rigi.gravityScale = 10;
+        }
+        else
+        {
+            rigi.gravityScale = 20;
+        }
+    }
     public void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Enemy"))
+        if (collision.gameObject.CompareTag("Enemy") && maxHealth > 0)
         {
             currentHealth--;
-
+            if (damageTime != true) { anim.Play("HeroDamage"); }
             healthBar.SetHealth(currentHealth);
         }
+
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Background"))
+           ground = true;
+    }
+    public void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Background"))
+        ground = false;
     }
 }
