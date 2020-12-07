4084cfd9a94be8a2c5f70d659a3488ef669e84ed
Ricky110926 <rduon451@live.kutztown.edu>
2020-12-06 22:18:20 -0500

    Final Build
    WERE FREEEEEEE

---
+++ The Underglow/Assets/Scripts/QuitScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class QuitScript : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKey("escape")) Application.Quit();
+
+    }
+}
